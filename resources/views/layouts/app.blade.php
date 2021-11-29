c4a0a9ba849369cd928704c2176764a6452558fc
stefanidmtrv <dimitrovastefani00@gmail.com>
2021-11-29 11:38:12 +0000

    adding a flash message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,17 +8,21 @@
     </head>
 <body>
     <h1>Posts Blog @yield('title')</h1>
+    
+        @if (session('message'))
+        <p><b>{{session('message')}}</b></p>
+        @endif
 
-@if ($errors->any())
-<div>
-    Errors:
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{$error}}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
+        @if ($errors->any())
+        <div>
+            Errors:
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
 
     <div>@yield('content')</div>
 </body>
